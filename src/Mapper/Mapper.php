138e1360208f03de09b4cd5bd796e9ca11d0092a
Rafael Pereira Girolineto <rafapg.85@gmail.com>
2018-09-25 11:01:34 -0300

    Conversão de tipos int, float, boolean e string

--- src/Mapper/Mapper.php
+++ src/Mapper/Mapper.php
@@ -30,7 +30,49 @@ class Mapper
     public function processConfig(string $config, string $targetPath)
     {
         $configValue = $this->configResolver->resolve($config);
-        $this->entity->set($targetPath, $configValue);
+        $target = $this->getTarget($targetPath);
+        $convertedValue = $this->convertValueToType($configValue, $target->type);
+
+        $this->entity->set($target->path, $convertedValue);
+    }
+
+    private function getTarget(string $targetPath) : \stdClass
+    {
+        $type = 'string';
+        $path = $targetPath;
+        if (preg_match('/(.*)\((.*)\)/', $targetPath)) {
+            $matches = [];
+            preg_match('/(.*)\((.*)\)/', $targetPath, $matches);
+            $path = $matches[1];
+            $type =  $matches[2];
+        }
+
+        $target = new \stdClass();
+        $target->path = $path;
+        $target->type = $type;
+
+        return $target;
+    }
+
+    private function convertValueToType(string $configValue, string $type)
+    {
+        $convertedValue = null;
+        switch ($type) {
+            case 'int':
+                $convertedValue = (int) $configValue;
+                break;
+            case 'float':
+                $convertedValue = (float) $configValue;
+                break;
+            case 'boolean':
+            case 'bool':
+                $convertedValue = ($configValue === 'true');
+                break;
+            default:
+                $convertedValue = (string) $configValue;
+                break;
+        }
+        return $convertedValue;
     }
 
     public function getEntity()
